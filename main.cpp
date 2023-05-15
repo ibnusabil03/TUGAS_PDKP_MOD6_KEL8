1d9c0b159c6e62a721c502c9f096ea36136b7186
ibnusabil03 <129574934+ibnusabil03@users.noreply.github.com>
2023-05-15 22:23:07 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
- #include <iostream>
+ 
+#include <iostream>
 #include <cmath>
 
 using namespace std;
@@ -7,6 +8,8 @@ class Calculator {
 protected:
     double numbers [100];
     int count;
+    double input;
+    double input1, input2;
 
 public:
     Calculator() : count (0){}
@@ -33,6 +36,25 @@ public:
         return result;
     }
 
+    double jumlah() {
+        while (true){
+                cout << "Masukkan angka (masukkan 'x' untuk berhenti): ";
+                while (cin >> input) {
+                        addNumber(input);
+                        cout << "Masukkan angka (masukkan 'x' untuk berhenti) : ";
+                }
+                if (cin.fail()) {
+                    string stopSignal;
+                    cin.clear();
+                    cin >> stopSignal;
+                        if (stopSignal == "x") {
+                            break;
+                        }
+                }
+        }
+        cout << "Hasil penjumlahan: " << getResult() << endl;
+    }
+
     double subtract() {
         double result = numbers[0];
         for (int i = 1; i < count ; i++) {
@@ -42,6 +64,25 @@ public:
         return result;
     }
 
+    double kurang() {
+        while (true){
+                cout << "Masukkan angka (masukkan 'x' untuk berhenti): ";
+                while (cin >> input) {
+                        addNumber(input);
+                        cout << "Masukkan angka (masukkan 'x' untuk berhenti) : ";
+                }
+                if (cin.fail()) {
+                    string stopSignal;
+                    cin.clear();
+                    cin >> stopSignal;
+                        if (stopSignal == "x") {
+                            break;
+                        }
+                }
+        }
+        cout << "Hasil penjumlahan: " << subtract() << endl;
+    }
+
     double multiply() {
         double result = 1;
         for (int i = 0; i < count; i++){
@@ -49,6 +90,25 @@ public:
         }
         clear();
         return result;
+    }
+
+    double kali() {
+        while (true){
+                cout << "Masukkan angka (masukkan 'x' untuk berhenti): ";
+                while (cin >> input) {
+                        addNumber(input);
+                        cout << "Masukkan angka (masukkan 'x' untuk berhenti) : ";
+                }
+                if (cin.fail()) {
+                    string stopSignal;
+                    cin.clear();
+                    cin >> stopSignal;
+                        if (stopSignal == "x") {
+                            break;
+                        }
+                }
+        }
+        cout << "Hasil penjumlahan: " << multiply() << endl;
     }
 
     double divide() {
@@ -63,6 +123,25 @@ public:
         }
         clear();
         return result;
+    }
+
+    double bagi() {
+        while (true){
+                cout << "Masukkan angka (masukkan 'x' untuk berhenti): ";
+                while (cin >> input) {
+                        addNumber(input);
+                        cout << "Masukkan angka (masukkan 'x' untuk berhenti) : ";
+                }
+                if (cin.fail()) {
+                    string stopSignal;
+                    cin.clear();
+                    cin >> stopSignal;
+                        if (stopSignal == "x") {
+                            break;
+                        }
+                }
+        }
+        cout << "Hasil penjumlahan: " << divide() << endl;
     }
 };
 
@@ -98,9 +177,26 @@ public:
         return sqrt(numbers[0]);
     }
 
+    double akarKuadrat() {
+        cout << "Masukkan angka: ";
+        cin >> input1;
+        addNumber(input1);
+        cout << "Hasil akar kuadrat: " << squareRoot() << endl;
+    }
+
     double exponentiation() {
         clear();
         return pow(numbers[0], numbers[1]);
+    }
+
+    double pangkat() {
+        cout << "Masukkan angka: ";
+        cin >> input1;
+        cout << "Masukkan angka: ";
+        cin >> input2;
+        addNumber(input1);
+        addNumber(input2);
+        cout << "Hasil pemangkatan: " << exponentiation () << endl;
     }
 
     double factorial() {
@@ -113,9 +209,23 @@ public:
         return result;
     }
 
+    double faktorial() {
+        cout << "Masukkan angka: ";
+        cin >> input1;
+        addNumber(input1);
+        cout << "Hasil faktorial: " << factorial() << endl;
+    }
+
     double sine() {
         clear();
         return Trigonometry::sine(numbers[0]);
+    }
+
+    double sin() {
+        cout << "Masukkan angka: ";
+        cin >> input1;
+        addNumber(input1);
+        cout << "Hasil sinus: " << sine() << endl;
     }
 
     double cosine() {
@@ -123,19 +233,32 @@ public:
         return Trigonometry::cosine(numbers[0]);
     }
 
+    double cos() {
+        cout << "Masukkan angka: ";
+        cin >> input1;
+        addNumber(input1);
+        cout << "Hasil cosinus: " << cosine() << endl;
+    }
+
     double tangent() {
         clear();
         return Trigonometry::tangent(numbers[0]);
+    }
+
+    double tan() {
+        cout << "Masukkan angka: ";
+        cin >> input1;
+        addNumber(input1);
+        cout << "Hasil tangent: " << tangent() << endl;
     }
 };
 
 int main() {
     BasicCalculator basicCalc;
     ScientificCalculator scientificCalc;
-    int kalkulator;
-    int operasi1;
-    int operasi2;
-    double input1, input2, input;
+    string kalkulator;
+    string operasi1;
+    string operasi2;
 
     cout << "===== Kalkulator Sederhana ======" << endl;
 
@@ -146,130 +269,48 @@ int main() {
         cout << "Pilih kalkulator : ";
         cin >> kalkulator;
 
-        if (kalkulator == 1) {
-            cout << " " << endl;
+        if (kalkulator[0] == 49) {
+            cout << endl;
             cout << "Menu Kalkulator Dasar" << endl;
             cout << "1. Penjumlahan\n2. Pengurangan\n3. Perkalian\n4. Pembagian" << endl;
             cout << "Pilih operasi : ";
             cin >> operasi1;
 
-            if (operasi1 == 1) {
-                while (operasi1 == 1){
-                    cout << "Masukkan angka (masukkan 'x' untuk berhenti): ";
-                    while (cin >> input) {
-                        basicCalc.addNumber(input);
-                        cout << "Masukkan angka (masukkan 'x' untuk berhenti) : ";
-                    }
-                    if (cin.fail()) {
-                        string stopSignal;
-                        cin.clear();
-                        cin >> stopSignal;
-                        if (stopSignal == "x") {
-                            break;
-                        }
-                    }
-                }
-                cout << "Hasil penjumlahan: " << basicCalc.getResult() << endl;
-
-            } else if (operasi1 == 2) {
-                while (operasi1 == 2) {
-                    cout << "Masukkan angka (masukkan 'x' untuk berhenti): ";
-                    while (cin >> input) {
-                        basicCalc.addNumber(input);
-                        cout << "Masukkan angka (masukkan 'x' untuk berhenti) : ";
-                    }
-                    if (cin.fail()) {
-                        string stopSignal;
-                        cin.clear();
-                        cin >> stopSignal;
-                        if (stopSignal == "x") {
-                            break;
-                        }
-                    }
-                }
-                cout << "Hasil pengurangan: " << basicCalc.subtract() << endl;
-            } else if (operasi1 == 3) {
-                while (operasi1 == 3) {
-                    cout << "Masukkan angka (masukkan 'x' untuk berhenti): ";
-                    while (cin >> input) {
-                        basicCalc.addNumber(input);
-                        cout << "Masukkan angka (masukkan 'x' untuk berhenti) : ";
-                    }
-                    if (cin.fail()) {
-                        string stopSignal;
-                        cin.clear();
-                        cin >> stopSignal;
-                        if (stopSignal == "x") {
-                            break;
-                        }
-                    }
-                }
-                cout << "Hasil perkalian: " << basicCalc.multiply() << endl;
-            } else if (operasi1 == 4) {
-                while (operasi1 == 4) {
-                    cout << "Masukkan angka (masukkan 'x' untuk berhenti): ";
-                    while (cin >> input) {
-                        basicCalc.addNumber(input);
-                        cout << "Masukkan angka (masukkan 'x' untuk berhenti) : ";
-                    }
-                    if (cin.fail()) {
-                        string stopSignal;
-                        cin.clear();
-                        cin >> stopSignal;
-                        if (stopSignal == "x") {
-                            break;
-                        }
-                    }
-                }
-                cout << "Hasil pembagian: " << basicCalc.divide() << endl;
+            if (operasi1[0] == 49) {
+                basicCalc.jumlah();
+            } else if (operasi1[0] == 50) {
+                basicCalc.kurang();
+            } else if (operasi1[0] == 51) {
+                basicCalc.kali();
+            } else if (operasi1[0] == 52) {
+                basicCalc.bagi();
             } else {
                 cout << "Operasi tidak valid." << endl;
             }
-        } else if (kalkulator == 2) {
-            cout << " " << endl;
+
+        } else if (kalkulator[0] == 50) {
+            cout <<  endl;
             cout << "Menu Kalkulator Saintifik" << endl;
             cout << "1. Akar Kuadrat\n2. Pangkat\n3. Faktorial\n4. Sin\n5. Cos\n6. Tan" << endl;
             cout << "Pilih operasi : ";
             cin >> operasi2;
 
-            if (operasi2 == 1) {
-                cout << "Masukkan angka: ";
-                cin >> input1;
-                scientificCalc.addNumber(input1);
-                cout << "Hasil akar kuadrat: " << scientificCalc.squareRoot() << endl;
-
-            } else if (operasi2 == 2) {
-                cout << "Masukkan angka: ";
-                cin >> input1;
-                cout << "Masukkan angka: ";
-                cin >> input2;
-                scientificCalc.addNumber(input1);
-                scientificCalc.addNumber(input2);
-                cout << "Hasil pemangkatan: " << scientificCalc.exponentiation () << endl;
-            } else if (operasi2 == 3) {
-                cout << "Masukkan angka: ";
-                cin >> input1;
-                scientificCalc.addNumber(input1);
-                cout << "Hasil faktorial: " << scientificCalc.factorial() << endl;
-            } else if (operasi2 == 4) {
-                cout << "Masukkan angka: ";
-                cin >> input1;
-                scientificCalc.addNumber(input1);
-                cout << "Hasil sine: " << scientificCalc.sine() << endl;
-            } else if (operasi2 == 5) {
-                cout << "Masukkan angka: ";
-                cin >> input1;
-                scientificCalc.addNumber(input1);
-                cout << "Hasil cosine: " << scientificCalc.cosine() << endl;
-            } else if (operasi2 == 6) {
-                cout << "Masukkan angka: ";
-                cin >> input1;
-                scientificCalc.addNumber(input1);
-                cout << "Hasil tangent: " << scientificCalc.tangent() << endl;
+            if (operasi2[0] == 49) {
+                scientificCalc.akarKuadrat();
+            } else if (operasi2[0] == 50) {
+                scientificCalc.pangkat();
+            } else if (operasi2[0] == 51) {
+                scientificCalc.faktorial();
+            } else if (operasi2[0] == 52) {
+                scientificCalc.sin();
+            } else if (operasi2[0] == 53) {
+                scientificCalc.cos();
+            } else if (operasi2[0] == 54) {
+                scientificCalc.tan();
             } else {
                 cout << "Operasi tidak valid." << endl;
             }
-        } else if (kalkulator == 3) {
+        } else if (kalkulator[0] == 51) {
             cout << "Terima kasih telah menggunakan kalkulator." << endl;
             break;
         } else {
@@ -279,4 +320,3 @@ int main() {
 
     return 0;
 }
-                
